edd33e2d137f44ff1a636798bd2bfc40d6d0400f
david1234098 <d29492731@gmail.com>
2026-09-22 00:04:59 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -24,12 +24,19 @@ app.post("/send-message", async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
+});
+    
 
     try {
         await transporter.sendMail({
