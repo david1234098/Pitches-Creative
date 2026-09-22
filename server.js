23c17a376a06f7079940479b534a7688ff5eb712
david1234098 <d29492731@gmail.com>
2026-09-22 18:12:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -25,9 +25,8 @@ app.post("/send-message", async (req, res) => {
     try {
         const { data, error } = await resend.emails.send({
     from: "onboarding@resend.dev",
-    to: "D29492731@gmail.com",
+    to: "d29492731@gmail.com",
             subject: `New message from ${name}`,
-            replyTo: email,
             text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
         });
 
